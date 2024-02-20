eea1f6acae3b35fce199565a15c4086456cb1639
Matus Kosut <matus.kosut@ntnu.no>
2024-02-20 11:15:57 +0100

    add vuetify

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,54 @@
-import { defineConfig } from "vitest/config";
-import vue from "@vitejs/plugin-vue"; // Import the plugin here
 
+import { fileURLToPath } from 'node:url';
+
+import {
+  configDefaults,
+  defineConfig,
+  // mergeConfig,
+} from 'vitest/config';
+
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
+// import viteConfig from './vite.config';
+
+/**
+ * Vitest Configure
+ *
+ * @see {@link https://vitest.dev/config/}
+ */
+// export default mergeConfig(
+//   viteConfig({ command: 'serve', mode: '' }),
+//   defineConfig({
+//   ...
+//   })
+// );
 export default defineConfig({
-  test: {
-    environment: "happy-dom",
-    globals: true,
+  // Resolver
+  resolve: {
+    // https://vitest.dev/config/#alias
+    alias: {
+      '@': fileURLToPath(new URL('./pages/.vitepress', import.meta.url)),
+      '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
-  plugins: [vue()], // Include it in your array of plugins here
-});
+  test: {
+    environment: 'happy-dom',
+    exclude: [...configDefaults.exclude],
+    globals: true,
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    // https://vuetifyjs.com/en/getting-started/unit-testing/
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
+  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    vuetify({ autoImport: true }),
+  ],
+})
