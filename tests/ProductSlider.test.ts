48b99ac5bceb7d8c48ae668fba0502bb4dc453b8
Matus Kosut <matus.kosut@ntnu.no>
2024-02-20 11:15:24 +0100

    add props

--- tests/ProductSlider.test.ts
+++ tests/ProductSlider.test.ts
@@ -3,9 +3,27 @@ import ProductSlider from "../pages/.vitepress/components/ProductSlider.vue";
 import { expect, test, it, describe } from "vitest";
 
 describe('ProductSlider', () => {
+  it('renders empty component', () => {
+    const wrapper = mount(ProductSlider, {
+      propsData: {
+        products: [],
+      }
+    });
+    expect(wrapper.exists()).toBe(true);
+  });
+
   it('renders the component', () => {
     const wrapper = mount(ProductSlider, {
-      products: [],
+      propsData: {
+        products: [
+          {
+            title: 'Product 1',
+            text: 'Describe product 1',
+            img: './product1.png',
+            href: '#product1',
+          },
+        ],
+      }
     });
     expect(wrapper.exists()).toBe(true);
   });
