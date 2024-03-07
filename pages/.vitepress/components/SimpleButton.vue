289657e34c15dcb2c63366c648e742f633a39620
Matus Kosut <matus.kosut@ntnu.no>
2024-03-07 10:30:04 +0100

    rename

--- pages/.vitepress/components/SimpleButton.vue
+++ pages/.vitepress/components/SimpleButton.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 
 export default {
-  // name: 'ContentHeader',
+  // name: 'SimpleButton',
   props: {
     title: { type: String, required: true },
     href: { type: String, default: "" },
@@ -43,6 +43,12 @@ export default {
         <template v-slot:append>
           <!-- :class="{'ml-1': isHovering, 'mr-1': !isHovering}" -->
           <v-icon color="#ef8114" style="margin-top: 1px !important"></v-icon>
+          <!-- <v-expand-x-transition>
+            <v-icon v-if="isHovering" class="mt-1" color="#ef8114"></v-icon>
+          </v-expand-x-transition>
+          <v-expand-x-transition>
+            <v-icon v-if="!isHovering" class="mt-1" color="#ef8114"></v-icon>
+          </v-expand-x-transition> -->
         </template>
       </v-btn>
     </template>
