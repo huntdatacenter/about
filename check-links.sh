8a6a632ccb57c498ca0732bec7fd3855a73da5c0
Matus Kosut <matus.kosut@ntnu.no>
2025-09-17 11:09:43 +0200

    add exceptions

--- check-links.sh
+++ check-links.sh
@@ -6,7 +6,9 @@ declare -a errors
 # 
 # bash check-links.sh | grep ERROR
 
-for link in $(find pages -type f -iname "*.md" -print0 | xargs -0 grep -hEo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort | uniq | tr "\n" " "); do 
+exceptions='^(https://www.linkedin.com/company/huntcloud/|https://www.linkedin.com/in/[^/]+/)$'
+
+for link in $(find pages -type f -iname "*.md" -print0 | xargs -0 grep -hEo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort | uniq | grep -vE "${exceptions}" |  tr "\n" " "); do 
   #| xargs -I @ bash -c "[[ \"$(curl -s -o /dev/null -w '%{http_code}' @)\" == \"404\" ]] && echo '@ ⚠️' || echo '@ ✅' "
   echo -n "${link}: "
   output=$(curl -s -o /dev/null -w '%{http_code}' "${link}")
